7fab74ad67d6372b10a1aa2ac68a4249bfe41687
Aleksander Paravac <watz@nerd2nerd.org>
2011-03-15 23:12:59 +0100

    Updated map-rendering code

--- map/map.php
+++ map/map.php
@@ -1,26 +1,40 @@
 <?php
-$map_data = file_get_contents("blubb.txt");
+// Read JSON data
+$map_data = file_get_contents("map.json");
+if (!$map_data) {
+  die("Could not file!\n");
+}
 $map = json_decode($map_data, TRUE);
 if (!$map) {
-	die ("Could not read map!\n");
+	die("Could not read map correctly!\n");
 }
+
+// Get Plantes
 $planets = $map['world']['planets'];
-/*foreach ($planets as $planet_data) {
-	foreach ($planet_data as $key => $value) {
-		echo $key."\t".$value."\t";
-	}
-	echo "\n";
-}*/
-/*echo "Size of image is: ".$map['world']['bounding-box']['xmax']." x ".$map['world']['bounding-box']['ymax']."\n";*/
-$img_size_x = $map['world']['bounding-box']['xmax'];
-$img_size_y = $map['world']['bounding-box']['ymax'];
+
+// Get World size
+$img_size_x = $map['world']['bounding-box']['xmax'] - $map['world']['bounding-box']['xmin'];
+$img_size_y = $map['world']['bounding-box']['ymax'] - $map['world']['bounding-box']['ymin'];
+
+// Create an image with GDlib
 $gd = imagecreatetruecolor($img_size_x, $img_size_y);
+if (!$gd) {
+  die("Can't create image via GDlib!\n");
+}
+
+// Color in RGB
 $img_color = imagecolorallocate($gd, 154, 255, 242);
+
+// Render Map
 foreach ($planets as $planet_data) {
 	$x = round($planet_data['x'], 0);
 	$y = round($planet_data['y'], 0);
 	echo "Setting pixel: ".$x.",".$y."\n";
+  // Place a filed circle on every planet position
 	imagefilledellipse($gd, $x, $y, 5, 5, $img_color);
 }
-imagepng($gd, "map.png");
+// Return Image as PNG
+if (!imagepng($gd, "map.png")) {
+  die("Can't create image map.png!\n");
+}
 ?>
