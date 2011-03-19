a34cb0d59408943e4f02405fa93fc0309b4a41a4
Petschge <mail@petschge.de>
2011-03-19 21:02:34 +0100

    Make IDs non-zero

--- gitter/json_output.c
+++ gitter/json_output.c
@@ -10,10 +10,10 @@ void json_output(cluster_t* planets, int n_planets) {
 	printf("{ \"world\":\n");
 	printf("  { \"bounding-box\": { \"xmin\": 0, \"xmax\": %d, \"ymin\": 0, \"ymax\": %d},\n", GLOBALS.WIDTH, GLOBALS.HEIGHT);
 	printf("    \"planets\": [\n");
-	for(i = 0; i < n_planets-1; i++) {
+	for(i = 1; i < n_planets; i++) {
 		printf("      {\"id\":%d, \"x\": %f, \"y\": %f, \"owner\": null},\n", i, planets[i].center.x, planets[i].center.y);
 	}
-	printf("      {\"id\":%d, \"x\": %f, \"y\": %f, \"owner\": null}\n", i, planets[n_planets-1].center.x, planets[n_planets-1].center.y);
+	printf("      {\"id\":%d, \"x\": %f, \"y\": %f, \"owner\": null}\n", i, planets[n_planets].center.x, planets[n_planets-1].center.y);
 	printf("    ],\n");
 	printf("    \"ships\": [ ],\n");
 	printf("    \"asteroids\": [ ],\n");
@@ -34,19 +34,19 @@ void json_update(waypoint_t* route1, waypoint_t* route2, int n_planets) {
 		printf("  { \"bounding-box\": { \"xmin\": 0, \"xmax\": %d, \"ymin\": 0, \"ymax\": %d},\n", GLOBALS.WIDTH, GLOBALS.HEIGHT);
 		printf("    \"ships\": [ \n");
 		if(t1) {
-			printf("      {\"id\": %d, \"x\": %f, \"y\": %f, \"owner\": 1, \"size\": 3, \"contents\": \"T  \", \"docked_to\": null}%s\n", n_planets+1, t1->point.x, t1->point.y, t2 ? ",": "");
+			printf("      {\"id\": %d, \"x\": %f, \"y\": %f, \"owner\": 1, \"size\": 3, \"contents\": \"T  \", \"docked_to\": null}%s\n", n_planets+2, t1->point.x, t1->point.y, t2 ? ",": "");
 		}
 		if(t2) {
-			printf("      {\"id\": %d, \"x\": %f, \"y\": %f, \"owner\": 2, \"size\": 6, \"contents\": \"TTLRR \", \"docked_to\": null}\n", n_planets+2, t2->point.x, t2->point.y);
+			printf("      {\"id\": %d, \"x\": %f, \"y\": %f, \"owner\": 2, \"size\": 6, \"contents\": \"TTLRR \", \"docked_to\": null}\n", n_planets+3, t2->point.x, t2->point.y);
 		}
 		if((t1 && ! t1->next) || (t2 && ! t2->next)) {
 			printf("    \"], \n");
 			printf("    \"explosions\": [\n");
 			if(t1 && ! t1->next) {
-				printf("      {\"id\": %d, \"x\": %f, \"y\": %f}%s\n", n_planets+1, t1->point.x, t1->point.y, (t2 && !t2->next) ? ",": "");
+				printf("      {\"id\": %d, \"x\": %f, \"y\": %f}%s\n", n_planets+2, t1->point.x, t1->point.y, (t2 && !t2->next) ? ",": "");
 			}
 			if(t2 && ! t2->next) {
-				printf("      {\"id\": %d, \"x\": %f, \"y\": %f}\n", n_planets+2, t2->point.x, t2->point.y);
+				printf("      {\"id\": %d, \"x\": %f, \"y\": %f}\n", n_planets+3, t2->point.x, t2->point.y);
 			}
 		}
 		printf("    ],\n");
