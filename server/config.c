515a5429449dd83031398ea6d0504cd67868d834
Urs Ganse <urs@nerd2nerd.org>
2011-05-29 16:16:36 +0200

    Global lua configuration file, as a seperate lua state.

---
+++ server/config.c
@@ -0,0 +1,79 @@
+/*
+ * Config-File reader (lua as well, of course)
+ */
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+#include <stdlib.h>
+#include "config.h"
+
+lua_State* config_state = 0;
+
+/* Initialize Configuration by reading a config file */
+int init_config_from_file(char* filename) {
+
+	/* warn if we already had a config */
+	if(config_state) {
+		CONFIG_WARNING("Warning: config_state already exists, overriding with new config from %s (possible memleak of old state?)\n", filename);
+	}
+
+	/* Create the lua state */
+	config_state = luaL_newstate();
+	if(config_state == NULL) {
+		fprintf(stderr, "Creating configuration lua state failed.\n");
+		exit(1);
+	}
+
+	/* Load all libraries (All of them, we trust our config files) */
+	luaL_openlibs(config_state);
+
+	/* Run the file's code */
+	if(luaL_dofile(config_state, filename)) {
+		fprintf(stderr, "Warning: Parsing config file %s failed:\n%s\n",filename, lua_tostring(config_state, -1));
+		return 0;
+	}
+
+	/* Return "true" */
+	return 1;
+}
+
+/* Get an integer value from the config */
+int config_get_int(char* param_name) {
+	
+	int i;
+	lua_getglobal(config_state, param_name);
+
+	/* If this parameter is no int, return 0 */
+	if(!lua_isnumber(config_state,-1)) {
+		lua_pop(config_state, 1);
+		CONFIG_DEBUG("Config parameter %s is not an int, returning 0\n", param_name);
+		return 0;
+	}
+
+	/* Otherwise, return it's value */
+	i = lua_tonumber(config_state, -1);
+	lua_pop(config_state, 1);
+
+	return i;
+}
+
+
+/* Get a string value from the config */
+const char* config_get_string(char* param_name) {
+	const char* s;
+	lua_getglobal(config_state, param_name);
+
+	/* If this parameter is no int, return 0 */
+	if(!lua_isstring(config_state,-1)) {
+		lua_pop(config_state, 1);
+		CONFIG_DEBUG("Config parameter %s is not a string, returning \"\"\n", param_name);
+		return 0;
+	}
+
+	/* Otherwise, return it's value */
+	s = lua_tostring(config_state, -1);
+	lua_pop(config_state, 1);
+
+	return s;
+
+}
---
+++ server/config.lua
@@ -0,0 +1,9 @@
+
+
+-- Maximum amount of cycles a ship may use per lua function call
+lua_max_cycles = 10000
+
+
+-- Name of the initial lua code file that will be executed before any player-
+-- specific code, in a new ship computer.
+ship_init_code_file = "init.lua"
