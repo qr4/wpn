a15c29d486d16b983fafbb7df6f1f2e54b3e2858
twist <twist_at_nerd2nerd.org>
2011-06-11 20:53:35 +0200

    umstellung auf pstr und dstr

--- net/pstr.c
+++ net/pstr.c
@@ -12,12 +12,47 @@
 // pstr (sowas wie pascal-strings nur mit fixer groesse)
 //
 
-int pstr_append(struct pstr* dest, char* src, int src_len) {
-  size_t len = src_len + dest->used > dest->size ? dest->size - dest->used : src_len;
-  memcpy(dest->str + dest->used, src, len);
-  dest->used += len;
+// pstr leer machen
+void pstr_clear(struct pstr* p) {
+  p->used = 0;
+}
+
+// pstr als char*
+char* pstr_as_cstr(struct pstr* p) {
+  return p->str;
+}
+
+void pstr_set(struct pstr* dest, struct pstr* src) {
+  memcpy(dest->str, src->str, src->used + 1);
+  dest->used = src->used;
+}
+
+// setzt src als string
+int pstr_set_cstr(struct pstr* p, char* src, int src_len) {
+  const size_t str_size = sizeof ((struct pstr*)0)->str - 1;  // -1 wegen \0-terminierung
+  size_t len = str_size < src_len ? str_size : src_len;
+  memcpy(p->str, src, len);
+  p->used = len;
+  p->str[len] = '\0';
   return len;
 }
+
+// an den pstr was drannhaengen
+int pstr_append(struct pstr* dest, char* src, int src_len) {
+  const size_t str_size = sizeof ((struct pstr*)0)->str - 1;  // -1 wegen \0-terminierung
+  size_t len = src_len + dest->used > str_size ? str_size - dest->used : src_len;
+  memcpy(dest->str + dest->used, src, len);
+  dest->used += len;
+  dest->str[dest->used] = '\0';
+  return len;
+}
+
+// laenge von dem pstr abfragen
+int pstr_len(struct pstr* p) {
+  return p->used;
+}
+
+
 
 
 //
@@ -26,7 +61,7 @@ int pstr_append(struct pstr* dest, char* src, int src_len) {
 
 
 // init dstr. malloc und sowas
-int pstr_malloc(struct dstr* p) {
+int dstr_malloc(struct dstr* p) {
   p->data = (char*)malloc(PSTR_DSTR_DEFAULT_SIZE);
   if (p->data == NULL) {
     return -1;
@@ -38,19 +73,47 @@ int pstr_malloc(struct dstr* p) {
 }
 
 // dstr wieder frei geben
-void pstr_free(struct dstr* p) {
+void dstr_free(struct dstr* p) {
   free(p->data);
 }
 
-// inhalt loeschen
-void pstr_clear(struct dstr* p) {
+// inhalt loeschen und puffer-groesse auf ein vertraegliches mass
+// zuruecksetzen (wenn noetig)
+void dstr_clear(struct dstr* p) {
   p->unused = 0;
   p->next = 0;
+  if (p->size > 800000) {
+    char* tmp = realloc(p->data, 500000);
+    if (tmp != NULL) {
+      p->data = tmp;
+    }
+  }
 }
 
 // dstr als \0-terminierten string zurueckgeben = bereich von next bis unused 
 char* dstr_as_cstr(struct dstr* p) {
   return p->data + p->next;
+}
+
+// setzt src als string
+int dstr_set(struct dstr* p, char* src, size_t src_len) {
+  size_t size = p->size;
+
+  if (size < src_len + 1) {
+    size = size * 2 > (src_len + 1) ? size * 2 : (src_len + 1); // + 1 wegen \0-terminierung
+    char* tmp = (char*)realloc(p->data, size);
+    if (tmp == NULL) {
+      return -1;
+    }
+    p->data = tmp;
+    p->size = size;
+  }
+  memcpy(p->data, src, src_len);
+  p->unused = src_len;
+  p->next = 0;
+  p->last_char = '\0';
+  p->data[src_len] = '\0';
+  return 0;
 }
 
 // fuegt src mit der laenge src_len an dstr an
@@ -90,6 +153,11 @@ int dstr_append(struct dstr* p, char* src, size_t src_len) {
 // wie oebn, nur hier ist src \0-terminiert
 int dstr_append_cstr(struct dstr* p, char* src) {
   return dstr_append(p, src, strlen(src));
+}
+
+// gibt die anzahl der belegten bytes zureuck
+int dstr_len(struct dstr* p) {
+  return p->unused - p->next;
 }
 
 // gibt die naechste zeile zurueck
