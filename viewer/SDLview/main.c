7be4aceac32eddcf26a78955a21e6f59c426a61d
twist <twistaffenerd2nerd.org>
2011-06-15 20:56:49 +0200

    der sdl-eumel spricht jetzt netzwerk :-)
    TODO: netzwerk wird byte fuer byte gelesen. das ist mehr als unschoen.
    mal mit petschge zusammen setzen und pstd/dstr verwenden

--- viewer/SDLview/main.c
+++ viewer/SDLview/main.c
@@ -1,3 +1,9 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <errno.h>
 #include <unistd.h>
 
@@ -5,6 +11,8 @@
 #include "json.h"
 #include "sdl.h"
 #include "types.h"
+
+#define PORT "8080"
 
 asteroid_t* asteroids;
 int n_asteroids, n_asteroids_max;
@@ -23,7 +31,7 @@ int n_shots, n_shots_max;
 
 bbox_t boundingbox;
 
-int checkInput(buffer_t* b) {
+int checkInput(int net, buffer_t* b) {
 	int ret;
 	fd_set inputset;
 	struct timeval timeout;
@@ -31,34 +39,39 @@ int checkInput(buffer_t* b) {
 	static char lc = 0; // Char der in der letzten Runde gelesen wurde
 
 	FD_ZERO(&inputset);
-	FD_SET(0, &inputset); // Add stdin to the (empty) set of input file descriptores
+	FD_SET(net, &inputset); // Add stdin to the (empty) set of input file descriptores
 
 	timeout.tv_sec = 0;
 	timeout.tv_usec = 100;
 
 	/* select returns 0 if timeout, 1 if input available, -1 if error. */
-	ret = select(FD_SETSIZE, &inputset, NULL, NULL, &timeout);
+	ret = select(net + 1, &inputset, NULL, NULL, &timeout);
 
 	if(ret == 1) {
 		// Input available
-		if(FD_ISSET(0, &inputset)) {
-			// And it's acutally on stdin \o/
-			char c = fgetc(stdin);
-			if(c == EOF) {
-				fprintf(stderr, "Stdin was closed. Goodbye.\n");
-				usleep(3e6);
-				return -1;
-			}
+		if(FD_ISSET(net, &inputset)) {
+
+      // TODO
+      // ich hasse den code!!!! armes netzwerk!
+      char c[1];
+      ssize_t len = recv(net, c, sizeof(c), 0);
+
+      if (len == 0) {
+        // handle server hang-up
+        fprintf(stderr, "server hang up. Goodbye.\n");
+        return -1;
+      }
+
 			if(b->fill >= b->size - 1) {
 				growBuffer(b);
 			}
-			b->data[b->fill] = c;
+			b->data[b->fill] = c[0];
 			b->fill++;
-			if(lc == '\n' && c == '\n') {
+			if(lc == '\n' && c[0] == '\n') {
 				// Wir haben eine Leerzeile, sprich ein JSON-Objekt sollte zu Ende sein
 				return 2;
 			}
-			lc = c;
+			lc = c[0];
 			return 1;
 		}
 		// War wohl falscher Alarm
@@ -71,6 +84,7 @@ int checkInput(buffer_t* b) {
 		} else {
 			// This is a real error
 			fprintf(stderr, "select() produced error %d\n", errno);
+      perror("select()");
 			return -1;
 		}
 	} else {
@@ -121,9 +135,71 @@ void allocStuff() {
 	n_shots_max = 1000;
 }
 
-int main() {
+// get sockaddr, IPv4 or IPv6:
+// TODO... das steht doch schon in der network.c
+void *get_in_addr(struct sockaddr *sa)
+{
+  if (sa->sa_family == AF_INET) {
+    return &(((struct sockaddr_in*)sa)->sin_addr);
+  }
+  return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int connect2server(char* server) {
+
+  int sockfd;
+  struct addrinfo hints, *servinfo, *p;
+  int rv;
+
+  bzero(&hints, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+
+  printf("connect to server %s\n", server);
+  rv = getaddrinfo(server, PORT, &hints, &servinfo);
+
+  if (rv != 0) {
+    printf("getaddrinfo: %s\n", gai_strerror(rv));
+    freeaddrinfo(servinfo); // all done with this structure
+    exit(EXIT_FAILURE);
+  }
+
+  // loop through all the results and connect to the first we can
+  for (p = servinfo; p != NULL; p = p->ai_next) {
+    if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+      perror("connect_to_server client: socket");
+      continue;
+    }
+
+    if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+      close(sockfd);
+      perror("connect_to_server client: connect");
+      continue;
+    }
+
+    break;
+  }
+
+  freeaddrinfo(servinfo); // all done with this structure
+
+  if (p == NULL) {
+    printf("connect_to_server client: failed to connect\n");
+    exit(EXIT_FAILURE);
+  }
+
+  return sockfd;
+}
+
+int main(int argc, const char* argv[] ) {
 	buffer_t* buffer = getBuffer();
 	int ret;
+
+  if (argc != 2) {
+    printf("%s <server-ip>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  int net = connect2server(argv[1]);
 
 	allocStuff();
 
@@ -141,7 +217,7 @@ int main() {
 
 		ret = 1;
 		while(ret == 1) {
-			ret = checkInput(buffer);
+			ret = checkInput(net, buffer);
 		}
 
 		if(ret < 0) {
@@ -150,7 +226,7 @@ int main() {
 		}
 
 		if(ret == 0) {
-			usleep(10000);
+			usleep(1000);
 		}
 
 		if(ret == 2) {
@@ -159,6 +235,8 @@ int main() {
 			clearBuffer(buffer);
 		}
 	}
+
+  close(net);
 
 	return 0;
 }
