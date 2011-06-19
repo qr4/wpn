4bdb52a6e6403ed0b0691a046659e03d433eb38e
Urs Ganse <urs@nerd2nerd.org>
2011-06-19 21:11:48 +0200

    Urs' temporary ship initcode for testing.

--- server/init.lua
+++ server/init.lua
@@ -6,6 +6,20 @@ ASTEROID  = 4;
 BASE      = 8;
 SHIP      = 16;
 
+-- The default behaviour when recieving data from a docked partner is to
+-- execute it right away. (This allows initial programming of a new ship)
+function on_incoming_data(d)
+	local f = loadstring(d);
+	f();
+end
+
+
+-- Override global functions which should not be available within spaceships
+--print = nil   -- Ships are not supposed to be able to write to stdout
+pcall = nil   -- Lua errors can't be caught, but lead to distruption of the ship
+
+
+--  ********* Everything below here is debugging code *********
 
 -- A default, out of the box autopilot arrival handler.
 function on_autopilot_arrived() 
@@ -14,8 +28,8 @@ function on_autopilot_arrived()
 	x = math.random(20000)
 	y = math.random(20000)
 	print("Going to "..x..", "..y)
-	-- set_autopilot_to(x,y)
-	moveto(x, y)
+	set_autopilot_to(x,y)
+	--moveto(x, y)
 end
 
 
@@ -29,7 +43,9 @@ function	on_docking_complete()
 		send_data(function()
 			print "Code transferred successfully!";
 			on_timer_expired = function()
-				print "My timer function was mogrified!"
+				x,y = get_position();
+				set_autopilot_to(4500,4500);
+				print("Flying at " .. x .. ", " .. y)
 				set_timer(3);
 			end
 		end)
@@ -45,59 +61,46 @@ function on_undocking_complete()
 end
 
 
--- The default behaviour when recieving data from a docked partner is to
--- execute it right away. (This allows initial programming of a new ship)
-function on_incoming_data(d)
-	local f = loadstring(d);
-	f();
-end
-
 -- And here is some verbose initialization code for testing:
 
-print "Hi, I'm a ship"
+-- print "Hi, I'm a ship"
 
-s = entity_to_string(self)
-print("Here is some information about myself:\n".. s)
-closest_planet = find_closest(600, PLANET)
-if(closest_planet) then
-	print("I'm very close to:\n".. entity_to_string(closest_planet))
-end
+-- s = entity_to_string(self)
+-- print("Here is some information about myself:\n".. s)
+-- closest_planet = find_closest(600, PLANET)
+-- if(closest_planet) then
+--	print("I'm very close to:\n".. entity_to_string(closest_planet))
+-- end
 
 -- print("And that guy belongs to player number "..get_player(find_closest(600,PLANET)));
 -- x,y = get_position(find_closest(600,PLANET));
 -- print("Also, his position is ".. x .. " " .. y);
 
-closest_ship = find_closest(600,SHIP);
-if(closest_ship) then
-	docked = dock(closest_ship);
-	print("Docking returned "..docked)
-
-else
-	print("No ship in 600 blocks radius.")
-end
+-- closest_ship = find_closest(600,SHIP);
+-- if(closest_ship) then
+-- 	docked = dock(closest_ship);
+-- 	print("Docking returned "..docked)
+-- else
+-- 	print("No ship in 600 blocks radius.")
+-- end
 
 -- For testing the execution time limit: an endless loop
 --while 1 do
 --	print("derping")
 --end
 
+
+-- Random rumfliegerei
 set_timer(3)
 function on_timer_expired()
-	print "My Alarm rang!"
-
 	-- set new timer
 	--set_timer(3)
-	set_autopilot_to(500, 500)
+	set_autopilot_to(math.random()*10000, math.random()*10000)
 	set_timer(1)
 	on_timer_expired = function()
 		x,y = get_position()
-		print("I'm now at "..x..", "..y)
+		-- print("I'm now at "..x..", "..y)
 		set_timer(1)
 	end
 	
 end
-
-
--- Override global functions which should not be available within spaceships
---print = nil   -- Ships are not supposed to be able to write to stdout
---pcall = nil   -- Lua errors can't be caught, but lead to distruption of the ship
