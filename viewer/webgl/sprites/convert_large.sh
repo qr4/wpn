09f11811bc969fce5dc1f3afd38450e2ef8f7af0
Simon Triphan <simon@niflheim.(none)>
2012-06-09 12:29:04 +0200

    Ships and Bases in one huge texture each

---
+++ viewer/webgl/sprites/convert_large.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#Die Default-Auflösung sind 300DPI.
+#Das entspricht 30x30 pixel für einen Raumschiff-Slot.
+#10px (die Inkscape-Einheit, die das Format unten
+#ausspuckt) entsprechen 1/9inch, d.h. bei 300DPI
+#entsprechen 30px im .svg 100 Pixeln im .png
+if [ -n "$1" ]; then
+	if [ "$1" -gt 0 ]; then
+		echo "Resolution $1 DPI selected"
+		RESOLUTION=$1
+	else
+		echo "Standard Resolution selected (100DPI)"
+		RESOLUTION=100
+	fi
+else
+	echo "Standard Resolution selected (100DPI)"
+	RESOLUTION=100
+fi
+#Die Namen von Schiffen/Basen bestehen aus 3 Teilen:
+#s_ar_6
+#Typ(s Schiff b Basis)_Serie_Anzahl an Slots
+#svg-Dateien mit den Schiffen/Basen drin
+SVGFILES="ships.svg bases.svg"
+if [ -e "slots.js" ]; then
+	rm slots.js
+fi
+for SVGFILE in $SVGFILES; do
+	echo "Exporting from $SVGFILE"
+	PNGFILE=`echo $SVGFILE | sed 's/svg/png/'`
+	cat tmp_objects | cut -d "," -f 1 | grep '[bs]_.*_[0-9]*$' > tmp_ships 
+	#Extrahiere die Positionen und Größen aller (relevanten) objekte
+	inkscape -S $SVGFILE | grep '.*_.*_.*' | sort > tmp_objects
+	if [ $SVGFILE == "ships.svg" ]; then
+		SVGNAME="s_all"
+	else
+		SVGNAME="b_all"
+	fi
+	#exportiere Schiffe/Basen in ein großes png
+	inkscape --export-png="$PNGFILE" --export-width="2048" --export-id="$SVGNAME" --export-id-only "$SVGFILE"
+	#Berechne die relativen Positionen der slots in pixeln
+	#und schreibe das Ergebnis in die Datei 'slots'
+	./get_textures.py $RESOLUTION
+done
+mv slots.js ..
---
+++ viewer/webgl/sprites/get_textures.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+import sys
+from math import ceil
+resolution=100/90.0#default: 300 DPI
+if len(sys.argv)>1:
+	resolution=int(sys.argv[1])/90.0
+f=open('tmp_objects','r')
+objects=f.readlines()
+f.close()
+f=open('slots.js','aw')
+#Arrays definieren
+f.write('var block_positions_str=new Array();\n');
+f.write('var ship_tex_sizes_str= new Array();\n');
+f.write('var ship_tex_pos_str= new Array();\n');
+objects=map(lambda x: x.strip().split(','),objects)#In Spalten aufteilen
+i=0;
+slot_x=list()
+slot_y=list()
+while (i<len(objects)):
+	if (objects[i][0].count('_')==2):#Zwei _: Eintrag fuer ein Schiff
+		ship_x=float(objects[i][1])
+		ship_y=float(objects[i][2])
+		ship_w=ceil(float(objects[i][3]))
+		num_slots=int(objects[i][0].split('_')[-1])#letzter Eintrag: Anzahl an slots
+		s_x=list()
+		s_y=list()
+		s='block_positions_str[\''+(objects[i][0])+'\']=['
+		for j in range(num_slots):
+			x=(float(objects[i+j+1][1])-ship_x)*resolution
+			y=(float(objects[i+j+1][2])-ship_y)*resolution
+			s_x=x
+			s_y=y
+			if (j>0):
+				s+=','
+			s+=('%f,%f' % (x,y))
+		s+=('];\n')
+		f.write(s)
+		slot_x.append(s_x)
+		slot_y.append(s_y)
+		i+=num_slots
+		f.write('ship_tex_sizes_str[\'%s\']=%3d;\n'%(objects[i][0],ship_w))
+		f.write('ship_tex_pos_str[\'%s\']=[%3d,%3d];\n'%(objects[i][0],ceil(ship_x),ceil(ship_y)))
+	i+=1
+f.close()
